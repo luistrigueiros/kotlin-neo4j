916260d6c916959e0bdd45e56a136f7d69670479
Luis Trigueiros <luistrigueiros@gmail.com>
2016-09-24 23:22:13 +0100

    Refactor to use lambas

--- src/main/kotlin/FirstTest.kt
+++ src/main/kotlin/FirstTest.kt
@@ -1,5 +1,19 @@
 import org.neo4j.driver.v1.AuthTokens
 import org.neo4j.driver.v1.GraphDatabase
+import org.neo4j.driver.v1.Session
+
+
+val createRecords = {session: Session ->
+    session.run("CREATE (a:Person {name:'Arthur', title:'King'})")
+}
+
+val displayRecords = { session : Session ->
+    val result = session.run("MATCH (a:Person) WHERE a.name = 'Arthur' RETURN a.name AS name, a.title AS title")
+    while (result.hasNext()) {
+        val record = result.next()
+        println(record.get("title").asString() + " " + record.get("name").asString())
+    }
+}
 
 
 fun main(args: Array<String>) {
@@ -7,13 +21,8 @@ fun main(args: Array<String>) {
     val driver = GraphDatabase.driver("bolt://192.168.99.100", AuthTokens.basic("neo4j", "admin"))
     val session = driver.session()
 
-    session.run("CREATE (a:Person {name:'Arthur', title:'King'})")
-
-    val result = session.run("MATCH (a:Person) WHERE a.name = 'Arthur' RETURN a.name AS name, a.title AS title")
-    while (result.hasNext()) {
-        val record = result.next()
-        println(record.get("title").asString() + " " + record.get("name").asString())
-    }
+    createRecords(session)
+    displayRecords(session)
 
     session.close()
     driver.close()
